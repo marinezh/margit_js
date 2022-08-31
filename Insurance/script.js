d04685693297b0318f97a85a304739ef19cd1183
student <student@BC-minimac022s-Mac-mini.local>
2022-08-31 11:25:32 +0300

    added insuranse task

---
+++ Insurance/script.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("form");
+
+const customer = document.querySelector("#name");
+
+const age = document.querySelector("#age");
+
+let customerName = customer.value;
+console.log(customerName);
+
+const riskCalc = () => {
+  let lowRisk = 500;
+  riskScore = 0;
+  if (age < 18) {
+    riskScore === lowRisk;
+  }
+};
