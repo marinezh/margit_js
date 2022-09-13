99328577f2b19a17fe50ee76b8161713467f16a2
Zhivotova Marina <s2200724@BC-C02J64AEQ6LR-MacBook-Air.local>
2022-09-13 13:48:38 +0300

    js part added to gradient gen

--- bg_generator/script.js
+++ bg_generator/script.js
@@ -4,11 +4,28 @@ let color1 = document.querySelector("#color1");
 let color2 = document.querySelector("#color2");
 
 let direction = document.querySelectorAll('input[name="direction"]');
-
+console.log(direction);
 let text = document.querySelector("p");
 
 const setGradient = (e) => {
   e.preventDefault();
   let selectedValue;
-  
+
+  for (const sel of direction) {
+    if (sel.checked) {
+      selectedValue = sel.value
+    }
+  }
+
+  let gradient = `Linear-gradient(to ${selectedValue}, ${color1.value}, ${color2.value})`;
+
+console.log(gradient);
+
+document.body.style.background = gradient;
+text.textContent = gradient + ';';
+
 };
+
+
+
+form.addEventListener('change', setGradient);
