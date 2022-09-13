540009d20e60b1715aa82de760be52756a95a241
student <student@BC-minimac022s-Mac-mini.local>
2022-09-13 11:05:51 +0300

    BG_gen_livicoding

---
+++ bg_generator/script.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("form");
+
+let color1 = document.querySelector("#color1");
+let color2 = document.querySelector("#color2");
+
+let direction = document.querySelectorAll('input[name="direction"]');
+
+let text = document.querySelector("p");
+
+const setGradient = (e) => {
+  e.preventDefault();
+  let;
+};
