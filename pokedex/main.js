8f37905e113c93a9ebbd1b37f5ae5e396bc394ea
Marina Zhivotova <mazhivotova@gmail.com>
2022-10-25 01:41:44 +0300

    add type symbols to the card and search func is done

--- pokedex/main.js
+++ pokedex/main.js
@@ -4,8 +4,16 @@ let searchInput = document.querySelector("#search");
 let pokeData = [];
 let filterPoke = [];
 
+// const getPokeType = (type) => {
+//   switch (type) {
+//     case "bug": 
+//       return <img src = ""></img> 
+//   }
+// }
+
+
 const fetchPokemon = () => {
-  for (let i = 1; i <= 50; i++) {
+  for (let i = 1; i <= 300; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     fetch(url)
       .then((res) => res.json())
@@ -13,8 +21,13 @@ const fetchPokemon = () => {
         const pokemon = {
           name: data.name,
           id: data.id,
+          gen: data.gen,
           image: data.sprites.other["official-artwork"].front_default,
-          type: data.types.map((type) => type.type.name).join(", "),
+          // type: data.types.map((type) => type.type.name).join(", "),
+          type: data.types.map((type) => `<img class="poke_symbol" src="assets/${type.type.name}.png"
+          alt="#"
+      />`)
+            .join(" "),
         };
         pokeData.push(pokemon);
         displayPokemon(pokeData);
@@ -26,11 +39,12 @@ const displayPokemon = (data) => {
   const cards = data
     .map(
       (pokeman) => `<div class="card">
-          <div class="image">
+          <div class="poke_image">
           <img src="${pokeman.image}" alt="#">
           </div>
-          <h2>${pokeman.name}</h2>
-          <p>${pokeman.id}. ${pokeman.type}</p>
+          <h2>#${pokeman.id} ${pokeman.name}</h2>
+          <div class = 'type'>${pokeman.type}</div>
+         
         </div>`
     )
     .join("");
@@ -42,15 +56,11 @@ fetchPokemon();
 const searchFunc = () => {
   console.log(searchInput.value);
 
-  let sort = pokeData.filter((elem) => elem.name.includes(searchInput.value));
-  console.log(sort);
+  let sort = pokeData.filter((elem) =>
+    elem.name.includes(searchInput.value) || elem.type.includes(searchInput.value))
+  // console.log(sort);
   displayPokemon(sort);
 };
 
-// if (searchInput.value == "") {
-//   content.innerHTML = displayPokemon(pokeData);
-// } else {
-//   content.innerHTML = sort;
-// }
 
 searchInput.addEventListener("input", searchFunc);
