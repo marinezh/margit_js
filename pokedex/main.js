f440e9e4e7d24ebb4f3399682b2fecaaec29c695
Marina Zhivotova <mazhivotova@gmail.com>
2022-10-26 22:15:12 +0300

    add gen search function

--- pokedex/main.js
+++ pokedex/main.js
@@ -1,32 +1,52 @@
 const content = document.querySelector(".pokedex");
-let searchInput = document.querySelector("#search");
+const searchInput = document.querySelector("#search");
+const buttons = document.querySelectorAll(".btn");
 
 let pokeData = [];
 let filterPoke = [];
 
 // const getPokeType = (type) => {
 //   switch (type) {
-//     case "bug": 
-//       return <img src = ""></img> 
+//     case "bug":
+//       return <img src = ""></img>
 //   }
 // }
 
+const getGen = (id) => {
+  if (id < 150) return 1;
+  else if (id < 300) return 2;
+  else return 3;
+};
+
+const getImg = (sprites) => {
+  let res = sprites.other.dream_world.front_default;
+  if (!res) res = sprites.other["official-artwork"].front_default;
+  return res;
+};
 
 const fetchPokemon = () => {
-  for (let i = 1; i <= 300; i++) {
+  for (let i = 1; i <= 905; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
+        let gen = getGen(data.id);
+        let img = getImg(data.sprites);
         const pokemon = {
           name: data.name,
           id: data.id,
-          gen: data.gen,
-          image: data.sprites.other["official-artwork"].front_default,
+          gen: gen,
+          image: img,
+          /* image: data.sprites.other["official-artwork"].front_default, */
           // type: data.types.map((type) => type.type.name).join(", "),
-          type: data.types.map((type) => `<img class="poke_symbol" src="assets/${type.type.name}.png"
+          type: data.types
+            .map(
+              (
+                type
+              ) => `<img class="poke_symbol" src="assets/${type.type.name}.png"
           alt="#"
-      />`)
+      />`
+            )
             .join(" "),
         };
         pokeData.push(pokemon);
@@ -56,11 +76,25 @@ fetchPokemon();
 const searchFunc = () => {
   console.log(searchInput.value);
 
-  let sort = pokeData.filter((elem) =>
-    elem.name.includes(searchInput.value) || elem.type.includes(searchInput.value))
+  let sort = pokeData.filter(
+    (elem) =>
+      elem.name.includes(searchInput.value) ||
+      elem.type.includes(searchInput.value)
+  );
   // console.log(sort);
   displayPokemon(sort);
 };
 
-
 searchInput.addEventListener("input", searchFunc);
+
+buttons.forEach((button, i) =>
+  button.addEventListener("click", () => {
+    console.log(i, ' clicked');
+    let sort;
+    if (!i) sort = pokeData;
+    else {
+      sort = pokeData.filter((pokemon) => (pokemon.gen === i));
+    }
+    displayPokemon(sort);
+  })
+);
